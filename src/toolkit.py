876b43f5a65d94226bfdd20870ea2ef77d046da7
cybersecurity-dev <174958202+cybersecurity-dev@users.noreply.github.com>
2026-02-13 22:40:46 +0100

    Create toolkit.py
    ++added file to sha256 calculator function

---
+++ src/toolkit.py
@@ -0,0 +1,11 @@
+import os
+import hashlib
+
+from pathlib import Path
+
+def calculate_sha256(fpath : Path):
+    sha256_hash = hashlib.sha256()
+    with open(fpath, "rb") as f:
+        for byte_block in iter(lambda: f.read(4096), b""):
+            sha256_hash.update(byte_block)
+    return sha256_hash.hexdigest()
