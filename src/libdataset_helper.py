2159dad1f2d0ef28ebbc6a875ab0166b06557990
Cyber Threat Defense Lab. <174958202+cybersecurity-dev@users.noreply.github.com>
2026-05-04 21:38:15 +0200

    ++added split_dataframe_wdate function

--- src/libdataset_helper.py
+++ src/libdataset_helper.py
@@ -1,1 +1,10 @@
-
+def split_dataframe_wdate(param_df     : pd.DataFrame, 
+                          date_col     : str   = "first_seen_utc", 
+                          label_col    : str   = "label", 
+                          train_ratio  : float = 0.5, 
+                          val_ratio    : float = 0.2, 
+                          test_ratio   : float = 0.3, 
+                          random_state : int = 42) \
+    -> Tuple[pd.DataFrame | None, pd.DataFrame | None, pd.DataFrame | None]:
+    
+    return None, None, None
